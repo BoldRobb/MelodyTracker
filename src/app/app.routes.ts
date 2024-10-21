61f1d1d28faa1980cfdb5c36e1ba9cb2fc7368a8
JazielHeredia <148661084+Jazielillo@users.noreply.github.com>
2024-10-20 23:00:36 -0700

    NavBars y Welcome Responsivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { NavbarUnloginComponent } from './components/navbars/navbar-unlogin/navbar-unlogin.component';
 import { NavbarLoginComponent } from './components/navbars/navbar-login/navbar-login.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 export const routes: Routes = [
-    { path: '', component: NavbarLoginComponent},
-    { path: 'asd', component: NavbarUnloginComponent},
-    { path: '**', pathMatch: 'full', redirectTo: ''}
+    { path: 'navbarlogin', component: NavbarLoginComponent},
+    { path: 'navbarunlogin', component: NavbarUnloginComponent},
+    { path: 'welcome', component: WelcomeComponent},
+    { path: '**', pathMatch: 'full', redirectTo: 'welcomelogin'}
 ];
